3df51d2e78cf24e25add2428660cdbce5551fa72
BenjaminPerrin <perrin.benjamin77@gmail.com>
2018-05-04 07:30:17 +0200

    gMap

--- client/app/contact/contact.component.ts
+++ client/app/contact/contact.component.ts
@@ -3,10 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  styleUrls: ['./contact.component.css'],
 })
 export class ContactComponent implements OnInit {
-
+  lat = 48.473821;
+  long = 1.01192;
   constructor() { }
 
   ngOnInit() {
