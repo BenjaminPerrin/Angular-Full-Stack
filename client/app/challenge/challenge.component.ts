bc9d2e68268423b15e7d09f2bf3b28fe874d63b0
BenjaminPerrin <perrin.benjamin77@gmail.com>
2018-05-04 07:40:09 +0200

    fix rainbow

--- client/app/challenge/challenge.component.ts
+++ client/app/challenge/challenge.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-challenge',
   templateUrl: './challenge.component.html',
-  styleUrls: ['./challenge.component.scss']
+  styleUrls: ['./challenge.component.scss'],
 })
 export class ChallengeComponent implements OnInit {
 
